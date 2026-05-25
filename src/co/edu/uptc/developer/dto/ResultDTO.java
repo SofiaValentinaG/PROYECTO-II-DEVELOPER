c8e28efd0ea7b21e9ed5cd23432eec6d024c985e
angeth0613 <angeth0613@gmail.com>
2026-05-25 09:03:46 -0500

    creación y corrección de clases

--- src/co/edu/uptc/developer/dto/ResultDTO.java
+++ src/co/edu/uptc/developer/dto/ResultDTO.java
@@ -1,9 +1,43 @@
 package co.edu.uptc.developer.dto;
 
 public class ResultDTO {
-	private boolean isSuccesfull;
-	private String message;
-	private 
-	
+    private boolean isSuccessful;
+    private String message;
+    private Object data;
+
+    public ResultDTO(boolean isSuccessful, String message, Object data) {
+        this.isSuccessful = isSuccessful;
+        this.message = message;
+        this.data = data;
+    }
+
+	public boolean isSuccessful() {
+		return isSuccessful;
+	}
+
+	public void setSuccessful(boolean isSuccessful) {
+		this.isSuccessful = isSuccessful;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		return "ResultDTO [isSuccessful=" + isSuccessful + ", message=" + message + ", data=" + data + "]";
+	}
 
 }
